0c8991a07d2bdb1265c3ea3eee0a1f17ab1e5764
Nazar Tolstoy <mobidik460@gmail.com>
2026-08-10 12:16:03 +0300

    remove legacy front

--- frontend-legacy/src/App.test.js
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import SamurayApp from "./App";
-it("renders without crashing", () => {
-  const div = document.createElement("div");
-  ReactDOM.render(<SamurayApp />, div);
-  ReactDOM.unmountComponentAtNode(div);
-});
